7136575ade3e3c679045cc27671cec09d0831c01
Victor Ng <vng@brandeis.edu>
2020-12-05 19:15:02 -0600

    update

--- fetch.py
+++ fetch.py
@@ -1,1 +1,23 @@
-import request
+import requests
+
+id_counter = 0
+
+for user in user_list:
+    r = requests.get('https://api.jikan.moe/v3/user/{user}/animelist/all'.format(user=user))
+    anime_list = r.json()
+    anime_list = anime_list['anime']
+
+    with open("user.txt", "w") as f:
+        f.write(user, id_counter)
+
+    with open("filename.txt", "w") as f:
+        for anime in anime_list:
+            f.write((anime['mal_id'], anime['title'], anime['score']))
+
+    
+    id_counter += 1
+
+
+
+#step 1 fetch all anime in dataset and ID and remap the ids into dictionary
+#using a user, search all his anime then add keypairs
---
+++ mal_scraper.py
@@ -0,0 +1,17 @@
+from lxml import html
+import requests
+import time
+import random
+
+if __name__ == "__main__":
+    for i in range(1000):
+        link = 'https://myanimelist.net/users.php?lucky=1'
+        page = requests.get(link)
+        tree = html.fromstring(page.content)
+        names = tree.xpath('//td[@align="center"]/div/a/text()')
+
+        with open("user.txt", "a") as f:
+            for name in names:
+                f.write(name+ " " + '\n')
+
+        time.sleep(random.randint(5, 10))
