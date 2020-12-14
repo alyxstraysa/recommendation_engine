d004f20e0e5b6cf6c0768aae654ad0bbd59956ac
wesleyng <wesleyng@tamu.edu>
2020-12-14 10:29:33 -0600

    Adding basic DB setup for site. Usage for SQLite3. Model files generated and need to be migrated before usage

--- anime_rec_website/anime_app/admin.py
+++ anime_rec_website/anime_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Anime_Pref, Anime_User
 
 # Register your models here.
+admin.site.register(Anime_Pref)
+admin.site.register(Anime_User)
+
--- anime_rec_website/anime_app/apps.py
+++ anime_rec_website/anime_app/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class PollsConfig(AppConfig):
-    name = 'polls'
+class AnimeRecConfig(AppConfig):
+    name = 'anime_app'
