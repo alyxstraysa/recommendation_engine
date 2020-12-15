6d1fa439926a42e7433057d66f883f8745675c77
wesleyng <wesleyng@tamu.edu>
2020-12-14 21:10:19 -0600

    Adding Templated Views to support main page and user recommendation. Have some random pictures for showcase

--- anime_rec_website/anime_app/views.py
+++ anime_rec_website/anime_app/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+from .models import Anime_User
 
 # Create your views here.
 
 def index(request):
-    template = loader.get_template('anime_app/index.html')
-    context={}
-    return HttpResponse(template.render(context, request))
+    #context={}
+    return render(request, 'anime_app/index.html')
 
 def user_rec(request, user_id, rank_id):
-    return HttpResponse("You have just entered the %s recommendation for %s" % (rank_id, user_id))
+    # try:
+    #     user = Anime_User.objects.get(pk=user_id)
+    # except Anime_User.DoesNotExist:
+    #     raise Http404('User ID does not exist')
+    # return HttpResponse("You have just entered the %s recommendation for %s" % (rank_id, user_id))
+    #user_id = get_object_or_404(Anime_User, pk=user_id)
+    return render(request, 'anime_app/user_rec.html', {'user_id': user_id})
