b2916a50a212d177ae7fb9ede455cbc7443bf1b4
Victor Ng <vng@brandeis.edu>
2020-12-16 11:50:41 -0600

    corrected infer function

--- LightGCN-PyTorch/code/main.py
+++ LightGCN-PyTorch/code/main.py
@@ -37,15 +37,18 @@ else:
     w = None
     world.cprint("not enable tensorflowboard")
 
-try:
-    for epoch in range(world.TRAIN_epochs):
-        start = time.time()
-        if epoch %10 == 0:
-            cprint("[TEST]")
-            Procedure.Test(dataset, Recmodel, epoch, w, world.config['multicore'])
-        output_information = Procedure.BPR_train_original(dataset, Recmodel, bpr, epoch, neg_k=Neg_k,w=w)
-        print(f'EPOCH[{epoch+1}/{world.TRAIN_epochs}] {output_information}')
-        torch.save(Recmodel.state_dict(), weight_file)
-    
+if world.config['model'] == 'train':
+    try:
+        for epoch in range(world.TRAIN_epochs):
+            start = time.time()
+            if epoch %10 == 0:
+                cprint("[TEST]")
+                Procedure.Test(dataset, Recmodel, epoch, w, world.config['multicore'])
+            output_information = Procedure.BPR_train_original(dataset, Recmodel, bpr, epoch, neg_k=Neg_k,w=w)
+            print(f'EPOCH[{epoch+1}/{world.TRAIN_epochs}] {output_information}')
+            torch.save(Recmodel.state_dict(), weight_file)
+elif world.config['model'] == 'infer':
     print("Inferring on predict:")
     Procedure.Infer(dataset, Recmodel, 0)
+else:
+    print("Invalid mode selected!")
