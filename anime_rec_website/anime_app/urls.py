bf6445f61faaf3068d80682debcb596b44bc0db9
wesleyng <wesleyng@tamu.edu>
2020-12-18 20:06:16 -0600

    Changes

--- anime_rec_website/anime_app/urls.py
+++ anime_rec_website/anime_app/urls.py
@@ -1,8 +1,78 @@
 from django.urls import path
 from . import views
+import requests
+
+ANIME_DICT = {}
+USER_ANIMES = {}
+
+def getRemappedAnime(user_animes):
+    #Returns the list of remapped anime IDS
+    output_list = []
+    for anime_id in user_animes:
+        try:
+            output_list.append(anime_dict[str(anime_id)])
+        except:
+            pass
+    
+    return output_list
+    
+    
+def setUp():
+    with open("./LightGCN-PyTorch/data/anime/anime.txt") as f:
+        next(f)
+        for line in f.readlines():
+                lineModified = line.split(" ")
+                anime, animeID = lineModified[0].strip('\n'), lineModified[1].strip('\n')
+                ANIME_DICT[animeID] = anime
+
+    
+    with open("./LightGCN-PyTorch/data/anime/train.txt") as f:
+        next(f)
+        for line in f.readlines():
+            lineModified = line.split(" ")
+            user, animes = lineModified[0].strip('\n'), [int(x.strip('\n')) for x in lineModified[1:]]
+            USER_ANIMES[user] = animes
+
+def getLevensteinDistance(user_animes):
+    #(Count, User)
+    maxSimilarity = (0, None)
+    lastUserID = 0
+    for user,animes in USER_ANIMES.items():
+        count = 0
+        for anime in user_animes:
+            if int(anime) in animes:
+                count += 1
+        if count > maxSimilarity[0]:
+            maxSimilarity = (count, user)
+        lastUserID = user
+    # for user, animes in USER_ANIMES.items():
+    #     simCalc = lev.similarity(user_animes, animes)
+    #     print(simCalc)
+    #     if simCalc > maxSimilarity[0]:
+    #         maxSimilarity = (simCalc, user)
+    if maxSimilarity == (0, None):
+        return lastUserID
+    return maxSimilarity[1]
+
+def testRequest(user):
+    r = requests.get('https://api.jikan.moe/v3/user/{user}/animelist/all'.format(user=user.strip()))
+    req_json = r.json()
+    anime_list = []
+    for anime in req_json['anime']:
+        anime_list.append(anime['mal_id'])
+
+    user_remap_animes = getRemappedAnime(anime_list)
+    return user_remap_animes
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:user_id>/', views.user_rec, name='user_rec'),
     path('anime_view', views.anime_view, name='anime_view')
 ]
+
+# if __name__ == '__main__':
+#     test_animes = testRequest('exorchids')
+#     setUp()
+#     remap = getRemappedAnime(test_animes)
+#     simUser = getLevensteinDistance(remap)
+#     print(simUser)
--- anime_rec_website/anime_app/views.py
+++ anime_rec_website/anime_app/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404
 from .models import Anime_User
 from .mal_retriever import get_Anime_Info
+from .urls import getRemappedAnime
+
 
 # Create your views here.
 
@@ -18,14 +20,24 @@ def user_rec(request, user_id):
     # return HttpResponse("You have just entered the %s recommendation for %s" % (rank_id, user_id))
     #user_id = get_object_or_404(Anime_User, pk=user_id)
     context = {'user_id': user_id, 'names': [], 'images': []}
+    r = requests.get('https://api.jikan.moe/v3/user/{user}/animelist/all'.format(user=user.strip()))
+    req_json = r.json()
+    anime_list = []
+    for anime in req_json['anime']:
+        anime_list.append(anime['mal_id'])
+
+    user_remap_animes = getRemappedAnime(anime_list)
+    
+    
+
     recommendations = [39617,39617,39617,39617,39617,39617,39617,39617,39617]
     for anime_ID in recommendations:
         anime_name, anime_image = get_Anime_Info(anime_ID)
-        context['names'].append(anime_name)
+        context['names'].append('')
         context['images'].append(anime_image)
     return render(request, 'anime_app/user_rec.html', context)
 
 def anime_view(request):
     context = {}
-    print("Requesting information for {mal_un}".format(mal_un = request.POST.mal_username))
+    print("Requesting information for {mal_un}".format(mal_un = request.POST.get('mal_username', '')))
     return render(request, 'anime_app/user_rec.html', context)
