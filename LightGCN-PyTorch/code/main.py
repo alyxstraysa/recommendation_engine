687b2f4a01b9d0ed9c854bbaed6226952eb647ef
Victor Ng <vng@brandeis.edu>
2020-12-20 13:52:53 -0600

    Update main.py

--- LightGCN-PyTorch/code/main.py
+++ LightGCN-PyTorch/code/main.py
@@ -51,6 +51,6 @@ if world.config['mode'] == 'train':
         pass
 elif world.config['mode'] == 'infer':
     print("Inferring on predict:")
-    Procedure.Infer(dataset, Recmodel, epoch, w, world.config['multicore'])
+    Procedure.Infer(dataset, Recmodel, 5373)
 else:
     print("Invalid mode selected!")
